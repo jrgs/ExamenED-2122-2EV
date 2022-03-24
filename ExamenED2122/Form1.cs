5a651628691ba420fe356cee9495759a429ebb60
Monica Garcia <monigar78@gmail.com>
2022-03-24 19:40:38 +0100

    Cambios en el codigo para su correcto funcionamiento

--- ExamenED2122/Form1.cs
+++ ExamenED2122/Form1.cs
@@ -11,7 +11,7 @@ namespace ExamenED2122
 {
     public partial class Form1 : Form
     {
-        double cantidadTotal;
+        double JSMDAWcantidadTotal;
         public Form1()
         {
             InitializeComponent();
@@ -19,20 +19,21 @@ namespace ExamenED2122
 
         private void btCalcular_Click(object sender, EventArgs e)
         {
-            double importe = Convert.ToDouble(txtImporte.Text);
-            double IVA = 0.21;
+            double JSMDAWimporte = Convert.ToDouble(txtImporte.Text);
+            double JSMDAWIVA = 0.21;
+            
             if (rbReducido.Checked == true)  // IVA reducido, 10%
-                IVA = 0.1;
+                JSMDAWIVA = 0.1;
             if (rbSuper.Checked == true)  // IVA super reducido, 4%
-                IVA = 0.4;
-            importe = importe * IVA; // Calculamos el importe con IVA
-            cantidadTotal = importe; // Se lo añadimos a la cantidad total
-            txtResultado.Text = Convert.ToString(importe);
+                JSMDAWIVA = 0.04;
+            JSMDAWimporte = (JSMDAWimporte * JSMDAWIVA) + JSMDAWimporte; // Calculamos el importe con IVA
+            JSMDAWcantidadTotal = JSMDAWimporte + JSMDAWcantidadTotal; // Se lo añadimos a la cantidad total
+            txtResultado.Text = Convert.ToString(JSMDAWimporte);
         }
 
         private void btTotal_Click(object sender, EventArgs e)
         {
-            txtResultado.Text = Convert.ToString(cantidadTotal);
+            txtResultado.Text = Convert.ToString(JSMDAWcantidadTotal);
         }
     }
 }
