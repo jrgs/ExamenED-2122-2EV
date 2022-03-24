c80c4e37148b3a9a6a967156225b09c54f8ac082
sanchez19558 <sanchez19558@iesmarenostrum.com>
2022-03-24 19:04:44 +0100

    Codigo Corregido Jens Byhan

--- ExamenED2122/Form1.cs
+++ ExamenED2122/Form1.cs
@@ -11,7 +11,7 @@ namespace ExamenED2122
 {
     public partial class Form1 : Form
     {
-        double cantidadTotal;
+        double cantidadTotalJPBC;
         public Form1()
         {
             InitializeComponent();
@@ -19,20 +19,20 @@ namespace ExamenED2122
 
         private void btCalcular_Click(object sender, EventArgs e)
         {
-            double importe = Convert.ToDouble(txtImporte.Text);
-            double IVA = 0.21;
+            double importeJPBC = Convert.ToDouble(txtImporte.Text);
+            double IVAJPBC = 0.21;
             if (rbReducido.Checked == true)  // IVA reducido, 10%
-                IVA = 0.1;
+                IVAJPBC = 0.1;
             if (rbSuper.Checked == true)  // IVA super reducido, 4%
-                IVA = 0.4;
-            importe = importe * IVA; // Calculamos el importe con IVA
-            cantidadTotal = importe; // Se lo añadimos a la cantidad total
-            txtResultado.Text = Convert.ToString(importe);
+                IVAJPBC = 0.04;
+            importeJPBC = importeJPBC + (importeJPBC * IVAJPBC); // Calculamos el importe con IVA
+            cantidadTotalJPBC = cantidadTotalJPBC + importeJPBC; // Se lo añadimos a la cantidad total
+            txtResultado.Text = Convert.ToString(importeJPBC);
         }
 
         private void btTotal_Click(object sender, EventArgs e)
         {
-            txtResultado.Text = Convert.ToString(cantidadTotal);
+            txtResultado.Text = Convert.ToString(cantidadTotalJPBC);
         }
     }
 }
