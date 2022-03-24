180368b304076e5f98c334248cab16d210377cfb
joaquincelis <celis20987@iesmarenostrum.com>
2022-03-24 20:17:11 +0100

    Codigo corregido

--- ExamenED2122/Form1.cs
+++ ExamenED2122/Form1.cs
@@ -11,7 +11,7 @@ namespace ExamenED2122
 {
     public partial class Form1 : Form
     {
-        double cantidadTotal;
+        double cantidadTotal_VTM_2122;
         public Form1()
         {
             InitializeComponent();
@@ -19,20 +19,22 @@ namespace ExamenED2122
 
         private void btCalcular_Click(object sender, EventArgs e)
         {
-            double importe = Convert.ToDouble(txtImporte.Text);
-            double IVA = 0.21;
-            if (rbReducido.Checked == true)  // IVA reducido, 10%
-                IVA = 0.1;
-            if (rbSuper.Checked == true)  // IVA super reducido, 4%
-                IVA = 0.4;
-            importe = importe * IVA; // Calculamos el importe con IVA
-            cantidadTotal = importe; // Se lo añadimos a la cantidad total
-            txtResultado.Text = Convert.ToString(importe);
-        }
+           
+            double importe_VTM2122 = Convert.ToDouble(txtImporte.Text)
+            double IVA_VTM_2122 = 0.21; 
 
+            if (rbReducido.Checked == true)  // IVA reducido, 10%
+                IVA_VTM_2122 = 0.1;
+            if (rbSuper.Checked == true)  // IVA super reducido, 4%
+                IVA_VTM_2122 = 0.04;
+            importe_VTM2122 = importe_VTM2122 + importe_VTM2122 * IVA_VTM_2122; // Calculamos el importe con IVA
+            cantidadTotal_VTM_2122 = cantidadTotal_VTM_2122 + importe_VTM2122; // Se lo añadimos a la cantidad totalll
+            txtResultado.Text = Convert.ToString(importe_VTM2122);
+        }
+        
         private void btTotal_Click(object sender, EventArgs e)
         {
-            txtResultado.Text = Convert.ToString(cantidadTotal);
+            txtResultado.Text = Convert.ToString(cantidadTotal_VTM_2122);
         }
     }
 }
