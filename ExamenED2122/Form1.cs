58f5b28848452cc20cddb87211f6d835fc2269d6
apn2122 <pamies20828@iesmarenostrum.com>
2022-03-24 19:52:42 +0100

    Exmanen ED

--- ExamenED2122/Form1.cs
+++ ExamenED2122/Form1.cs
@@ -11,7 +11,7 @@ namespace ExamenED2122
 {
     public partial class Form1 : Form
     {
-        double cantidadTotal;
+        double cantidadTotalAPÑ2122;
         public Form1()
         {
             InitializeComponent();
@@ -19,20 +19,25 @@ namespace ExamenED2122
 
         private void btCalcular_Click(object sender, EventArgs e)
         {
-            double importe = Convert.ToDouble(txtImporte.Text);
-            double IVA = 0.21;
+            double importeAPÑ2122 = Convert.ToDouble(txtImporte.Text);
+            double IVAAPÑ2122 = 0.21;
             if (rbReducido.Checked == true)  // IVA reducido, 10%
-                IVA = 0.1;
+                IVAAPÑ2122 = 0.1;
             if (rbSuper.Checked == true)  // IVA super reducido, 4%
-                IVA = 0.4;
-            importe = importe * IVA; // Calculamos el importe con IVA
-            cantidadTotal = importe; // Se lo añadimos a la cantidad total
-            txtResultado.Text = Convert.ToString(importe);
+                                          // Correción código 4% es 0.04
+                IVAAPÑ2122 = 0.04;
+            // Corrección código
+            importeAPÑ2122 = importeAPÑ2122 * (1 + IVAAPÑ2122); // Calculamos el importe con IVA
+
+            // Corrección código
+            cantidadTotalAPÑ2122 = cantidadTotalAPÑ2122 + importeAPÑ2122; // Se lo añadimos a la cantidad total
+
+            txtResultado.Text = Convert.ToString(importeAPÑ2122);
         }
 
         private void btTotal_Click(object sender, EventArgs e)
         {
-            txtResultado.Text = Convert.ToString(cantidadTotal);
+            txtResultado.Text = Convert.ToString(cantidadTotalAPÑ2122);
         }
     }
 }
