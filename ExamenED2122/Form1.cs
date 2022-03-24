a5299904b578d8c1cbf4f510d96102362165cb44
perez20839 <perez20839@iesmarenostrum.com>
2022-03-24 19:53:31 +0100

    JFPC-Ejercicio examen José Fco Pérez Castillo con errores lógicos corregidos

--- ExamenED2122/Form1.cs
+++ ExamenED2122/Form1.cs
@@ -11,7 +11,7 @@ namespace ExamenED2122
 {
     public partial class Form1 : Form
     {
-        double cantidadTotal;
+        double cantidadTotalJFPC;
         public Form1()
         {
             InitializeComponent();
@@ -19,20 +19,20 @@ namespace ExamenED2122
 
         private void btCalcular_Click(object sender, EventArgs e)
         {
-            double importe = Convert.ToDouble(txtImporte.Text);
-            double IVA = 0.21;
+            double importeJFPC = Convert.ToDouble(txtImporte.Text);
+            double IVA_JFPC = 0.21;
             if (rbReducido.Checked == true)  // IVA reducido, 10%
-                IVA = 0.1;
+                IVA_JFPC = 0.1;
             if (rbSuper.Checked == true)  // IVA super reducido, 4%
-                IVA = 0.4;
-            importe = importe * IVA; // Calculamos el importe con IVA
-            cantidadTotal = importe; // Se lo añadimos a la cantidad total
-            txtResultado.Text = Convert.ToString(importe);
+                IVA_JFPC = 0.04;
+            importeJFPC = importeJFPC + (importeJFPC * IVA_JFPC); // Calculamos el importe con IVA
+            cantidadTotalJFPC = cantidadTotalJFPC + importeJFPC; // Se lo añadimos a la cantidad total
+            txtResultado.Text = Convert.ToString(importeJFPC);
         }
 
         private void btTotal_Click(object sender, EventArgs e)
         {
-            txtResultado.Text = Convert.ToString(cantidadTotal);
+            txtResultado.Text = Convert.ToString(cantidadTotalJFPC);
         }
     }
 }
