1c5f57dc6f5942812eee8349860fb0f1f52e5207
Rober Vidal <vidal20617@iesmarenostrum.com>
2022-03-24 19:12:04 +0100

    Se corrigen los errores logicos detectados (3) en el programa, AO1CFSY

--- ExamenED2122/Form1.cs
+++ ExamenED2122/Form1.cs
@@ -11,7 +11,7 @@ namespace ExamenED2122
 {
     public partial class Form1 : Form
     {
-        double cantidadTotal;
+        double cantidadTotalAO1CFSY;
         public Form1()
         {
             InitializeComponent();
@@ -19,20 +19,20 @@ namespace ExamenED2122
 
         private void btCalcular_Click(object sender, EventArgs e)
         {
-            double importe = Convert.ToDouble(txtImporte.Text);
-            double IVA = 0.21;
+            double importeAO1CFSY = Convert.ToDouble(txtImporte.Text);
+            double IVAAO1CFSY = 0.21;
             if (rbReducido.Checked == true)  // IVA reducido, 10%
-                IVA = 0.1;
+                IVAAO1CFSY = 0.1;
             if (rbSuper.Checked == true)  // IVA super reducido, 4%
-                IVA = 0.4;
-            importe = importe * IVA; // Calculamos el importe con IVA
-            cantidadTotal = importe; // Se lo añadimos a la cantidad total
-            txtResultado.Text = Convert.ToString(importe);
+                IVAAO1CFSY = 0.04;
+            importeAO1CFSY = importeAO1CFSY + importeAO1CFSY * IVAAO1CFSY; // Calculamos el importe con IVA
+            cantidadTotalAO1CFSY = cantidadTotalAO1CFSY + importeAO1CFSY; // Se lo añadimos a la cantidad total
+            txtResultado.Text = Convert.ToString(importeAO1CFSY);
         }
 
         private void btTotal_Click(object sender, EventArgs e)
         {
-            txtResultado.Text = Convert.ToString(cantidadTotal);
+            txtResultado.Text = Convert.ToString(cantidadTotalAO1CFSY);
         }
     }
 }
