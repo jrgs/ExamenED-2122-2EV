0d2a86a095a051a5de27ebf0fa1217e41f01b262
PabloSirCanyada <psirvent78@gmail.com>
2022-03-24 19:48:37 +0100

    CCC2122 ED

--- ExamenED2122/Form1.cs
+++ ExamenED2122/Form1.cs
@@ -11,7 +11,7 @@ namespace ExamenED2122
 {
     public partial class Form1 : Form
     {
-        double cantidadTotal;
+        double CCC2122cantidadTotal;
         public Form1()
         {
             InitializeComponent();
@@ -19,20 +19,27 @@ namespace ExamenED2122
 
         private void btCalcular_Click(object sender, EventArgs e)
         {
-            double importe = Convert.ToDouble(txtImporte.Text);
-            double IVA = 0.21;
+            double CCC2122importe = Convert.ToDouble(txtImporte.Text);
+            double CCC2122IVA = 1.21;//Para que sume el IVA hay que multiplicar por 1,21 
+                                     //Al multiplicar por 0,21 sólo nos dice a cuanto equival el IVA sin sumarlo
+
             if (rbReducido.Checked == true)  // IVA reducido, 10%
-                IVA = 0.1;
+                CCC2122IVA = 1.1;
+
             if (rbSuper.Checked == true)  // IVA super reducido, 4%
-                IVA = 0.4;
-            importe = importe * IVA; // Calculamos el importe con IVA
-            cantidadTotal = importe; // Se lo añadimos a la cantidad total
-            txtResultado.Text = Convert.ToString(importe);
+                CCC2122IVA = 1.04;
+
+            CCC2122importe = CCC2122importe * CCC2122IVA; // Calculamos el importe con IVA
+
+            CCC2122cantidadTotal = CCC2122cantidadTotal + CCC2122importe; // Se lo añadimos a la cantidad total
+            //Pero ahora lo sumamos de verdad, hasta ahora le dabamos el valor de CCC2122Importe
+
+            txtResultado.Text = Convert.ToString(CCC2122importe);
         }
 
         private void btTotal_Click(object sender, EventArgs e)
         {
-            txtResultado.Text = Convert.ToString(cantidadTotal);
+            txtResultado.Text = Convert.ToString(CCC2122cantidadTotal);
         }
     }
 }
